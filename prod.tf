76ee1c9cbfc69342855d27844f2f4bd70a5dd729
ElijahLogan <elijahizelllogan@gmail.com>
2022-06-18 22:51:50 -0400

    default vpc with simple security network

--- prod.tf
+++ prod.tf
@@ -11,8 +11,8 @@ resource "aws_security_group" "prod_web"{
         cidr_blocks = ["0.0.0.0/0"]
     }
     ingress {
-        from_port = 80 
-        to_port = 80
+        from_port = 43 
+        to_port = 43
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
